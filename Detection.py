c146a3579009f72c282be07844696a0e2aae9f93
Subhashis Ghosh <141888234+subhashis-2003@users.noreply.github.com>
2023-09-10 06:50:51 +0530

    Update Detection.py
    Database added to store the inputs with time

--- Detection.py
+++ Detection.py
@@ -1,5 +1,19 @@
 import cv2
 import numpy as np
+import sqlite3
+from datetime import datetime
+
+# Initialize the SQLite database
+conn = sqlite3.connect('detection_database.db')
+cursor = conn.cursor()
+
+# Create a table to store detected objects
+cursor.execute('''CREATE TABLE IF NOT EXISTS object_detections
+                (timestamp DATETIME, object_label TEXT, confidence REAL)''')
+
+# Create a table to store detected faces
+cursor.execute('''CREATE TABLE IF NOT EXISTS face_detections
+                (timestamp DATETIME, x INT, y INT, width INT, height INT)''')
 
 # Load YOLOv3 model
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
@@ -45,6 +59,12 @@ def draw_boxes(frame, outs, classes):
                 cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
                 cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
+                # Store object detection in the database
+                timestamp = datetime.now()
+                cursor.execute("INSERT INTO object_detections (timestamp, object_label, confidence) VALUES (?, ?, ?)",
+                               (timestamp, label, confidence))
+                conn.commit()
+
 # Initialize the camera
 cap = cv2.VideoCapture(0)  # Use 0 for system camera, replace with the appropriate source for CCTV or other cameras
 
@@ -67,6 +87,12 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
+        # Store face detection in the database
+        timestamp = datetime.now()
+        cursor.execute("INSERT INTO face_detections (timestamp, x, y, width, height) VALUES (?, ?, ?, ?, ?)",
+                       (timestamp, x, y, w, h))
+        conn.commit()
+
     # Display the frame
     cv2.imshow('Video Feed', frame)
 
@@ -74,6 +100,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close all windows
+# Release the camera, close the database connection, and close all windows
 cap.release()
+conn.close()
 cv2.destroyAllWindows()
